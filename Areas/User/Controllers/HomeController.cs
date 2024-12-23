f633836adca8e3aa69892cb7d7ead51156b891b2
Ronakthesiya <ronakthesiya2004@gmail.com>
2024-12-23 07:55:05 +0530

    Add project files.

---
+++ Areas/User/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BOXCricket.Areas.User.Controllers
+{
+    [Area("User")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
