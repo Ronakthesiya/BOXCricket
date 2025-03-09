81ce4eb599c734063624f44e6b8e27b95c5b0273
Ronakthesiya <ronakthesiya2004@gmail.com>
2025-03-09 11:23:05 +0530

    Landing page edited

--- Areas/Authentication/Controllers/HomeController.cs
+++ Areas/Authentication/Controllers/HomeController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BOXCricket.Models;
+using BOXCricket.Services;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace BOXCricket.Areas.Authentication.Controllers
 {
     [Area("Authentication")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApiClientService _apiClientService;
+        private readonly string _apiBaseUrlVenue = "Venue";
+
+        public HomeController(ApiClientService apiClientService)
         {
+            _apiClientService = apiClientService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<VenueModel> venues = new List<VenueModel>();
+            ApiResponseModel response = await _apiClientService.GetAsync($"{_apiBaseUrlVenue}");
+            if (response.StatusCode != 200)
+            {
+                return NotFound(response.Message);
+            }
+
+            venues = JsonConvert.DeserializeObject<List<VenueModel>>(response.Data!.ToString());
+            ViewBag.Venues = venues;
 
             return View();
         }
